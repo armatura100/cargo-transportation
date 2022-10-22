0a4c0446583c1d7f4a69974fe6c4952c732135bb
Oleksandr <oleksandrtiumentsev@gmail.com>
2022-10-22 23:08:20 +0300

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,116 @@
+import * as flsFunctions from './modules/functions.js';
+flsFunctions.isWebp();
+
+import { langsArr } from './modules/languages.js';
+const allLangs = ['ru', 'en', 'fr'];
+const langElements = document.querySelectorAll('.languages__link');
+
+langElements.forEach(el => {
+    el.addEventListener('click', () => {
+        const langValue = el.dataset.lang;
+        location = window.location.pathname + '#' + langValue;
+        location.reload();
+    });
+});
+
+function changeLanguage() {
+    let hash = location.hash;
+    hash = hash.substring(1);
+
+    if (hash === '') {
+        document.querySelector(`.languages__link[data-lang="ru"]`).classList.add('active');
+    } else {
+        if (!allLangs.includes(hash)) {
+            location = window.location.pathname + '#ru';
+            location.reload();
+        }
+
+        document.querySelector('title').innerText = langsArr.pageTitle[hash];
+        for (let key in langsArr) {
+            const elements = document.querySelectorAll(`[data-lang-${key}]`);
+            if (elements.length > 0) {
+                elements.forEach(el => {
+                    el.innerText = langsArr[key][hash];
+                })
+            }
+        }
+        document.querySelector(`.languages__link[data-lang="${hash}"]`).classList.add('active');
+    }
+}
+
+changeLanguage();
+
+// showcase slider
+import Swiper, { Navigation, EffectFade, Autoplay } from 'swiper';
+const showcaseSlider = new Swiper('.showcase__slider', {
+    modules: [Navigation, EffectFade, Autoplay],
+    navigation: {
+        nextEl: '.showcase__slider .swiper-button-next',
+        prevEl: '.showcase__slider .swiper-button-prev',
+    },
+    speed: 500,
+    effect: 'fade',
+    fadeEffect: {
+        crossFade: true
+    },
+    autoplay: true,
+    loop: true,
+    autoHeight: true
+})
+
+// header and scrolls
+const header = document.querySelector('.header');
+
+const checkScroll = () => {
+    let scrollPos = window.scrollY;
+    if (scrollPos > 50) {
+        header.classList.add('scrolled');
+        document.body.style.setProperty('--header-pad', '70px')
+    } else {
+        header.classList.remove('scrolled');
+        document.body.style.setProperty('--header-pad', '145px')
+    }
+}
+
+window.addEventListener('scroll', checkScroll);
+document.addEventListener('DOMContentLoaded', checkScroll);
+
+// burger menu
+const menuBtn = document.querySelector('.menu__button');
+const menuList = document.querySelector('.menu__list');
+const menuLinks = document.querySelectorAll('.menu__link');
+const body = document.body;
+
+menuBtn.addEventListener('click', () => {
+    menuBtn.classList.toggle('active');
+    menuList.classList.toggle('active');
+    body.classList.toggle('lock');
+});
+
+menuLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        menuBtn.classList.remove('active');
+        menuList.classList.remove('active');
+        body.classList.remove('lock');
+    });
+});
+
+// append header items that don't fit into header to the burger menu
+const headerLocation = document.querySelector('.header__location-link');
+const appendedListItem = `
+<li class="menu__item">
+    <a class="menu__link">
+        ${headerLocation.innerText}
+    </a>
+</li>
+`
+if (window.matchMedia('(max-width: 26.25rem)').matches) {
+    menuList.insertAdjacentHTML('beforeend', appendedListItem);
+}
+
+// animation library
+import aos from 'aos';
+aos.init({
+    once: true,
+    duration: 800,
+});
---
+++ src/js/modules/languages.js
@@ -0,0 +1,215 @@
+export const langsArr = {
+    // page Title
+    pageTitle: {
+        ru: `Грузоперевозки`,
+        en: `Cargo Transportation`,
+        fr: `Transport de marchandises`
+    },
+
+    // header, menu
+    offices: {
+        ru: `Наши офисы`,
+        en: `Our offices`,
+        fr: `Nos bureaux`
+    },
+    services: {
+        ru: `услуги`,
+        en: `services`,
+        fr: `de service`
+    },
+    about: {
+        ru: `О компании`,
+        en: `about`,
+        fr: `sur`
+    },
+    vacancies: {
+        ru: `вакансии`,
+        en: `vacancies`,
+        fr: `postes vacants`
+    },
+    contacts: {
+        ru: `контакты`,
+        en: `contacts`,
+        fr: `Contacts`
+    },
+
+    // hero section
+    heroTitle: {
+        ru: `Грузовые перевозки`,
+        en: `Cargo transportation`,
+        fr: `Transport de marchandises`
+    },
+    heroText: {
+        ru: `Перевозка крупногабаритных грузов по Украине и Европе. Полное экспедирование, страхование, упаковка и хранение грузов`,
+        en: `Transportation of oversized cargo in Ukraine and Europe. Full forwarding, insurance, packaging and storage of goods`,
+        fr: `Transport de marchandises surdimensionnées en Ukraine et en Europe. Expédition complète, assurance, emballage et stockage des marchandises`
+    },
+
+    // showcase section (slider)
+    slideTitle1: {
+        ru: `грузоперевозки по европе`,
+        en: `Cargo transportation across Europe`,
+        fr: `Transport de marchandises à travers l'Europe`
+    },
+    slideText1: {
+        ru: `Страны Евросоюза, а также Украина. По росии не перевозим.`,
+        en: `EU countries and Ukraine as well. ruzzia is not included`,
+        fr: `Pays de l'UE, ainsi que l'Ukraine. Nous n'expédions pas à travers la ruzzie.`
+    },
+    slideTitle2: {
+        ru: `грузоперевозки по прибалтике`,
+        en: `Cargo transportation across Baltics`,
+        fr: `Transport de fret à travers les pays baltes`
+    },
+    slideText2: {
+        ru: `Литва, Латвия, Эстония, а также Норвегия и Швеция`,
+        en: `Lithuania, Latvia, Estonia, as well as Norway and Sweden`,
+        fr: `Lituanie, Lettonie, Estonie, ainsi que Norvège et Suède`
+    },
+    slideTitle3: {
+        ru: `жд перевозки по европе`,
+        en: `Railway transportation across Europe`,
+        fr: `Transport ferroviaire à travers l'Europe`
+    },
+    slideText3: {
+        ru: `Хорошо сотрудничаем с Польшей, Чехией, Словакией и Германией`,
+        en: `Good cooperation with Poland, Czech Republic, Slovakia and Germany`,
+        fr: `Bonne coopération avec la Pologne, la République tchèque, la Slovaquie et l'Allemagne`
+    },
+    slideTitle4: {
+        ru: `Морские перевозки`,
+        en: `Shipping overseas`,
+        fr: `Expédition`
+    },
+    slideText4: {
+        ru: `Доствляем через Балтийское море, Ботнический залив и Ла-Манш`,
+        en: `We deliver across the Baltic Sea, the Gulf of Bothnia and the English Channel`,
+        fr: `Nous livrons dans toute la mer Baltique, le golfe de Botnie et la Manche`
+    },
+    more: {
+        ru: `подробнее`,
+        en: `read more`,
+        fr: `Lire la suite`
+    },
+    prev: {
+        ru: `Назад`,
+        en: `prev`,
+        fr: `précédent`
+    },
+    next: {
+        ru: `след`,
+        en: `next`,
+        fr: `suivant`
+    },
+
+    // services section
+    service1: {
+        ru: `Авиаперевозки`,
+        en: `air transportation`,
+        fr: `transport aérien`
+    },
+    service2: {
+        ru: `Грузоперевозки`,
+        en: `Cargo transportation`,
+        fr: `Transport de marchandises`
+    },
+    service3: {
+        ru: `Морские перевозки`,
+        en: `Shipping overseas`,
+        fr: `Expédition`
+    },
+    service4: {
+        ru: `Мелкие перевозки`,
+        en: `Small transportation`,
+        fr: `Petit transport`
+    },
+    service5: {
+        ru: `жд перевозки`,
+        en: `Railway transportation`,
+        fr: `Transport ferroviaire`
+    },
+    service6: {
+        ru: `Доставка`,
+        en: `Delivery`,
+        fr: `Livraison`
+    },
+    service7: {
+        ru: `Подводные перевозки`,
+        en: `Underwater transportation`,
+        fr: `Transport sous-marin`
+    },
+    service8: {
+        ru: `Перевозки дронами`,
+        en: `Drone transportation`,
+        fr: `Transport par drone`
+    },
+
+    // about section
+    aboutText: {
+        ru: ` Брокеры помогут вам подсчитать и оптимизировать размеры таможенного сбора и составления соответствующих деклараций. Не обойтись без их участия при подготовке товаров на экспорт – импорт и подаче деклараций в соответствующие инстанции. Получение лицензий, сертификатов и других разрешительных документов, без участия брокеров, может оказаться непосильной задачей для многих клиентов. Остается отметить, что услуги брокера стоят совсем недорого, как и другие услуги от нашей компании.`,
+        en: `Brokers will help you calculate and optimize the amount of customs duty and the preparation of relevant declarations. You can not do without their participation in the preparation of goods for export - import and filing declarations with the appropriate authorities. Obtaining licenses, certificates and other permits, without the participation of brokers, can be an overwhelming task for many clients. It remains to be noted that the services of a broker are quite inexpensive, like other services from our company.`,
+        fr: `Des courtiers vous aideront à calculer et optimiser le montant des droits de douane et la préparation des déclarations correspondantes. Vous ne pouvez pas vous passer de leur participation à la préparation des marchandises à l'exportation - importation et au dépôt des déclarations auprès des autorités compétentes. L'obtention de licences, certificats et autres permis, sans la participation de courtiers, peut être une tâche écrasante pour de nombreux clients. Il reste à noter que les services d'un courtier sont assez peu coûteux, tout comme les autres services de notre entreprise.`
+    },
+
+    cardText1: {
+        ru: `офисов`,
+        en: `offices`,
+        fr: `des bureaux`
+    },
+    cardDescr1: {
+        ru: `В Европе и Украине`,
+        en: `Across EU and Ukraine`,
+        fr: `Dans l'UE et l'Ukraine`
+    },
+
+    cardText2: {
+        ru: `грузовиков`,
+        en: `trucks`,
+        fr: `camions`
+    },
+    cardDescr2: {
+        ru: `Volvo, Scania`,
+        en: `Volvo, Scania`,
+        fr: `Volvo, Scanie`
+    },
+
+    cardText3: {
+        ru: `сухогруза`,
+        en: `cargo-ships`,
+        fr: `cargo`
+    },
+    cardDescr3: {
+        ru: `Балтийское море, заливы`,
+        en: `Baltic sea, bays`,
+        fr: `mer Baltique, baies`
+    },
+
+    cardText4: {
+        ru: `вакансий`,
+        en: `vacancies`,
+        fr: `vacants`
+    },
+    cardDescr4: {
+        ru: `и дружный коллектив`,
+        en: `and friendly team`,
+        fr: `et équipe sympathique`
+    },
+
+    cardText5: {
+        ru: `сотрудников`,
+        en: `employees`,
+        fr: `des employés`
+    },
+    cardDescr5: {
+        ru: `профессионалов`,
+        en: `professionals`,
+        fr: `professionnels`
+    },
+
+    // footer
+    author: {
+        ru: `Разработано`,
+        en: `Developed`,
+        fr: `Développé par`
+    },
+}
